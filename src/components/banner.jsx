107d5a0ea986449349d7847ef3acfc18b50889d1
Brayan <brayan.may1999@gmail.com>
2026-05-22 18:45:08 -0300

    cambios hechos funciona perfecto

---
+++ src/components/banner.jsx
@@ -0,0 +1,17 @@
+import "./Banner.css";
+
+function Banner() {
+  return (
+    <section className="banner">
+      <div className="banner-content">
+        <h1>La diversión comienza aquí 🎉</h1>
+
+        <p>Descubre los mejores juguetes para todas las edades.</p>
+
+        <button>Ver catálogo</button>
+      </div>
+    </section>
+  );
+}
+
+export default Banner;
