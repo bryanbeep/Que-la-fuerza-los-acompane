d5deea512293e1d53dee65e39445e0dcca8bebaa
Brayan <brayan.may1999@gmail.com>
2026-05-22 18:11:35 -0300

    Update cards.jsx

---
+++ src/components/cards.jsx
@@ -0,0 +1,19 @@
+import "./Card.css";
+
+function Card({ image, title, price }) {
+  return (
+    <div className="card">
+      <img src={image} alt={title} />
+
+      <div className="card-body">
+        <h3>{title}</h3>
+
+        <p>${price}</p>
+
+        <button>Añadir al carrito</button>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
